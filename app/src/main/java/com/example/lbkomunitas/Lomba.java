71c6d06b15c5190a673d6d4a1939c1cdcc73efb0
LB Komunitas <lbkomunitas@gmail.com>
2019-07-05 08:38:40 +0700

    201907050830

--- app/src/main/java/com/example/lbkomunitas/Lomba.java
+++ app/src/main/java/com/example/lbkomunitas/Lomba.java
@@ -3,6 +3,7 @@ package com.example.lbkomunitas;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
@@ -16,11 +17,20 @@ public class Lomba extends AppCompatActivity
 {
 	
 	private static final String TAG = "Lomba";
+	private TextView lomba1, lomba2, ketlomba1, ketlomba2;
+	private String t1,t2,d1,d2;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lomba);
+
+		lomba1 = (TextView) findViewById(R.id.lomba1);
+        lomba2 = (TextView) findViewById(R.id.lomba2);
+        ketlomba1 = (TextView) findViewById(R.id.ketlomba1);
+        ketlomba2 = (TextView) findViewById(R.id.ketlomba2);
+
 		getLomba();
 	}
 
@@ -32,7 +42,21 @@ public class Lomba extends AppCompatActivity
                     @Override
             public void onResponse( Response<GetAllLombaQuery.Data> response)
                     {
-                    Log.d(TAG,"OnResponse: " + response.data().getAllLomba().get(0).lomba());
+                    Log.d(TAG,"OnResponse: " + response.data().getAllLomba().get(1).lomba());
+                        t1 = response.data().getAllLomba().get(1).lomba();
+                        t2 = response.data().getAllLomba().get(2).lomba();
+                        d1 = response.data().getAllLomba().get(1).ketlomba();
+                        d2 = response.data().getAllLomba().get(2).ketlomba();
+
+                        Lomba.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                lomba1.setText(t1);
+                                lomba2.setText(t2);
+                                ketlomba1.setText(d1);
+                                ketlomba2.setText(d2);
+                            }
+                        });
                     }
                     @Override
             public void onFailure(ApolloException e)
