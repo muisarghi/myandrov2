f0b32348e7cb0e9c9f38ad457ac31ce70c6053b6
LB Komunitas <lbkomunitas@gmail.com>
2019-07-09 15:49:47 +0700

    201907091600

--- app/src/main/java/com/example/lbkomunitas/Lomba.java
+++ app/src/main/java/com/example/lbkomunitas/Lomba.java
@@ -1,46 +1,49 @@
 package com.example.lbkomunitas;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.List;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.app.ProgressDialog;
+
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
-import java.util.ArrayList;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 //import javax.annotation.Nonnull;
 import android.widget.ListView;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
-import android.widget.SimpleAdapter;
-import android.widget.ListAdapter;
 import android.widget.ArrayAdapter;
-import static com.example.lbkomunitas.R.layout.list_item;
+
+import java.util.ArrayList;
 
 
+
+
+import static com.example.lbkomunitas.Constant.FIRST_COLUMN;
+import static com.example.lbkomunitas.Constant.SECOND_COLUMN;
+import static com.example.lbkomunitas.Constant.THIRD_COLUMN;
+import static com.example.lbkomunitas.Constant.FOURTH_COLUMN;
 
 public class Lomba extends AppCompatActivity
 {
 	
 	private static final String TAG = "Lomba";
-	private TextView lomba1, lomba2, ketlomba1, ketlomba2;
+	private TextView lomba1, lombax, ketlombax;
+    private String lomba2;
+    private TextView ketlomba1;
+    private String ketlomba2;
 	private String t1,t2,d1,d2;
 	private Integer i, jumlah;
 
     private static final String LOMBA_COLUMN = "LOMBA_COLUMN";
     private static final String KETLOMBA_COLUMN = "KETLOMBA_COLUMN";
-
+   // private ArrayList<LombaModel> lombaModelArrayList;
     private ListView listView;
-    ArrayAdapter<CharSequence> adapter;
-    ArrayAdapter<CharSequence> adapterb;
+   // ArrayAdapter<CharSequence> adapter;
+    //ArrayAdapter<CharSequence> adapterb;
+
+    private ArrayList<HashMap> list;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState)
@@ -48,15 +51,111 @@ public class Lomba extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lomba);
 
-		lomba1 = (TextView) findViewById(R.id.lombax);
+		//lomba1 = (TextView) findViewById(R.id.lombax);
+        lombax = (TextView) findViewById(R.id.lombax);
         //lomba2 = (TextView) findViewById(R.id.lomba2);
-        ketlomba1 = (TextView) findViewById(R.id.ketlombax);
+        ketlombax = (TextView) findViewById(R.id.ketlombax);
         //ketlomba2 = (TextView) findViewById(R.id.ketlomba2);
+        //listView = findViewById(R.id.listView);
 
-		getLomba();
+        //foodModelArrayList = new ArrayList<>();
+        //lombaModelArrayList = populateList();
+
+		//getLomba();
+
+        ListView lview = (ListView) findViewById(R.id.listView);
+        populateList();
+        listViewAdapter adapter = new listViewAdapter(this, list);
+        lview.setAdapter(adapter);
+
+
 	}
 
+    private void populateList() {
 
+        list = new ArrayList<HashMap>();
+
+        /*
+        HashMap temp = new HashMap();
+        temp.put(FIRST_COLUMN,"Colored Notebooks");
+        temp.put(SECOND_COLUMN, "By NavNeet");
+
+        list.add(temp);
+
+        HashMap temp1 = new HashMap();
+        temp1.put(FIRST_COLUMN,"Diaries");
+        temp1.put(SECOND_COLUMN, "By Amee Products");
+
+        list.add(temp1);
+
+        HashMap temp2 = new HashMap();
+        temp2.put(FIRST_COLUMN,"Note Books and Stationery");
+        temp2.put(SECOND_COLUMN, "By National Products");
+
+        list.add(temp2);
+
+        HashMap temp3 = new HashMap();
+        temp3.put(FIRST_COLUMN,"Corporate Diaries");
+        temp3.put(SECOND_COLUMN, "By Devarsh Prakashan");
+
+        list.add(temp3);
+
+        HashMap temp4 = new HashMap();
+        temp4.put(FIRST_COLUMN,"Writing Pad");
+        temp4.put(SECOND_COLUMN, "By TechnoTalaktive Pvt. Ltd.");
+
+        list.add(temp4);
+        */
+        MyApolloClient.getMyApolloCleint().query(
+                GetAllLombaQuery.builder().build()).enqueue(new ApolloCall.Callback<GetAllLombaQuery.Data>()
+        {
+            @Override
+            public void onResponse( Response<GetAllLombaQuery.Data> response)
+            {
+                jumlah = GetAllLombaQuery.GetAllLomba.$responseFields.length;
+                //GetAllLombaQuery.GetAllLomba feed = (GetAllLombaQuery.GetAllLomba) response.data().getAllLomba();
+
+                //for (GetAllLombaQuery.GetAllLomba feed : response.data().getAllLomba())
+
+
+                for (i = 0; i < jumlah; i++)
+
+                {
+                    String lombaxx=response.data().getAllLomba().get(i).lomba();
+                    String lombaxy=response.data().getAllLomba().get(i).ketlomba();
+                    HashMap <String, String> temp = new HashMap<String, String>();
+                    temp.put(FIRST_COLUMN, lombaxx.toString());
+                    temp.put(SECOND_COLUMN, lombaxy.toString());
+                    list.add(temp);
+                }
+
+                /*
+                for (i = 0; i < jumlah; i++)
+                {
+                    HashMap temp = new HashMap();
+                    //String mylomba=response.data().getAllLomba().get(i).lomba();
+                    //String myketlomba=response.data().getAllLomba().get(i).ketlomba()
+                    temp.put(FIRST_COLUMN, response.data().getAllLomba().get(i).lomba());
+                    temp.put(SECOND_COLUMN, response.data().getAllLomba().get(i).ketlomba());
+                    list.add(temp);
+                }
+                */
+            }
+
+
+            @Override
+            public void onFailure(ApolloException e)
+            {}
+
+
+        });
+
+
+
+    }
+
+
+/*
 	private void getLomba()
     {
         MyApolloClient.getMyApolloCleint().query(GetAllLombaQuery.builder().build()).enqueue
@@ -66,6 +165,8 @@ public class Lomba extends AppCompatActivity
             @Override
             public void onResponse( Response<GetAllLombaQuery.Data> response)
             {
+
+
                 final StringBuffer buffer = new StringBuffer();
                 for (GetAllLombaQuery.GetAllLomba feed : response.data().getAllLomba()) {
                     buffer.append(feed.lomba);
@@ -82,9 +183,12 @@ public class Lomba extends AppCompatActivity
                     {
                         lomba1.setText(buffer.toString());
 
+
                     }
 
                 });
+
+
 
 
             }
@@ -95,8 +199,7 @@ public class Lomba extends AppCompatActivity
     });
 
     }
-
-
+*/
 
     public void mLomba(View v)
     {
